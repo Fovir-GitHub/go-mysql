fde0138ed3ae9c0663101b30feb9b1273b674b28
Fovir <fovir@disroot.org>
2025-11-02 19:18:38 +0800

    feat: Add `albumByArtist` function.

--- main.go
+++ main.go
@@ -8,6 +8,13 @@ import (
 	"github.com/go-sql-driver/mysql"
 )
 
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
 var db *sql.DB
 
 func main() {
@@ -29,4 +36,31 @@ func main() {
 	}
 
 	fmt.Println("Connected!")
+
+	albums, err := alnumsByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
+}
+
+func alnumsByArtist(name string) ([]Album, error) {
+	var albums []Album
+
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumByArtist %q: %v", name, err)
+	}
+	return albums, nil
 }
