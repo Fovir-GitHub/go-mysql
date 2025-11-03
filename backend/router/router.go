337ce4252a224a1e840397c2ebfd9b75952057b7
Fovir <fovir@disroot.org>
2025-11-03 22:28:43 +0800

    feat: Allow `CORS`.

--- backend/router/router.go
+++ backend/router/router.go
@@ -2,11 +2,13 @@ package router
 
 import (
 	"github.com/Fovir-GitHub/go-mysql/handlers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter(albumHandler *handlers.AlbumHandler) *gin.Engine {
 	r := gin.Default()
+	r.Use(cors.Default())
 	r.GET("/albums/id/:id", albumHandler.GetByID)
 	r.GET("/albums/artist/:artist", albumHandler.GetByArtist)
 	r.GET("/albums/all", albumHandler.GetAllAlbums)
